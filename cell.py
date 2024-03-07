96781a0622ef5cf57b1689f62498f8e9b4420162
drstrangelove4 <158036236+drstrangelove4@users.noreply.github.com>
2024-03-07 17:53:13 +0000

    Update cell.py
    Split neighbour calculation into its own method and stored the results into a list so it is only calculated once instead of on every mainloop iteration. 
    
    This allows the program to do much less work when calculating whether a cell should be alive or dead.

--- cell.py
+++ cell.py
@@ -1,54 +1,58 @@
 from turtle import *
 import random
 
-
 # ----------------------------------------------------------------------------------------------------------------------
 
 # Constants
-CELL_GAP = 25
 CELL_SHAPE = "square"
 CELL_COLOR_DEAD = "black"
 CELL_COLOR_ALIVE = "white"
 ALIVE_INIT_CHANCE = int(100 / 20)
+
 
 # ----------------------------------------------------------------------------------------------------------------------
 
 
 class Cell(Turtle):
     """
-    A child class of Turtle. Each cell has 2 states: alive or dead. Cells can check the status of their 8 touching
-    neighbours and change their status depending on the rules of the game.
+    A child class of Turtle. Each cell has 2 states: 'alive' or 'dead' (white or black). Cells can check the status of
+    their (up to) 8 touching neighbours and change their status depending on the rules of the game.
     """
 
     # Any cell with <2 neighbours dies (turns black)
-    # Any cell with 2 or 3 neighbours lives (turns green)
+    # Any cell with 2 or 3 neighbours lives (turns white)
     # Any cell with >3 neighbours dies (turns black)
-    # Any cell with 3 neighbours comes alive (turns green)
+    # Any cell with 3 neighbours comes alive (turns white)
 
-    def __init__(self):
+    def __init__(self, x, y):
         super().__init__(CELL_SHAPE)
         self.color(CELL_COLOR_DEAD)
         self.alive = False
+
+        # Stores a list of adjacent cells. From this we can derive the amount of alive neighbours and thus the action
+        # of what this cell should take.
+        self.neighbours_list = []
         self.alive_neighbours = 0
+
+        # The position of the cell in the cell list used by the main function. Useful for calculating neighbours and
+        # reducing the amount of operations needed per loop.
+        self.x = x
+        self.y = y
+
+        # Sets its status to a random choice of alive/dead on creation.
         self.set_status()
 
     # ------------------------------------------------------------------------------------------------------------------
 
-    def check_neighbours(self, cell_list):
+    def check_neighbours(self):
         """
-        :param cell_list: list<cell objects>
+        Looks at the cells in the self.neighbours_list and counts how many are alive.
         :return:
-        Takes a list of cell objects and checks the amount of alive neighbour cells .
         """
-        for row in cell_list:
-            for cell in row:
-                # pass if we are dealing with ourselves.
-                if self.xcor() == cell.xcor() and self.ycor() == cell.ycor():
-                    pass
-                # Use the absolute value of the distance between cells to see if an alive cell is a neighbour
-                elif abs(cell.ycor() - self.ycor()) <= CELL_GAP and cell.alive:
-                    if abs(cell.xcor() - self.xcor()) <= CELL_GAP:
-                        self.alive_neighbours += 1
+
+        for cell in self.neighbours_list:
+            if cell.alive:
+                self.alive_neighbours += 1
 
     # ------------------------------------------------------------------------------------------------------------------
 
@@ -78,7 +82,7 @@ class Cell(Turtle):
 
     def set_status(self):
         """
-        Used to set initial cell status on game start.
+        Gives a cell X chance of being made "alive" when called.
         :return:
         """
         choice = random.randint(0, ALIVE_INIT_CHANCE)
@@ -86,3 +90,27 @@ class Cell(Turtle):
         if choice == 0:
             self.alive = True
             self.color(CELL_COLOR_ALIVE)
+
+    # ------------------------------------------------------------------------------------------------------------------
+
+    def find_neighbours(self, cell_list, gap):
+        """
+        Iterates over all the created cells and finds possible neighbours using distance between cell objects to
+        discriminate for valid neighbours.
+        :param gap: an int indicating the distance between cell centers.
+        :param cell_list: a list of cell objects containing all cells created by the game.
+        :return:
+        """
+
+        for row in cell_list:
+            for cell in row:
+
+                # pass if we are dealing with ourselves.
+                if self.xcor() == cell.xcor() and self.ycor() == cell.ycor():
+                    pass
+
+                # Use the absolute value of the distance between cells to see if an alive cell is a neighbour
+                elif abs(cell.ycor() - self.ycor()) <= gap:
+                    if abs(cell.xcor() - self.xcor()) <= gap:
+                        self.neighbours_list.append(cell)
+
